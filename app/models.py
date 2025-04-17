26985d76eedbe4ad66afeeea28de2442c9330537
kanedasec <vernon.acs@gmail.com>
2025-04-17 13:06:21 -0300

    adjust

--- app/models.py
+++ app/models.py
@@ -4,3 +4,4 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+SECRET= ADMIN123
